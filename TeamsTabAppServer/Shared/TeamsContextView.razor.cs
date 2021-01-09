649e83a08ee633e511eecee6ccd741f54988479e
Mika Berglund <mika.berglund@meteoriitti.com>
2021-01-09 18:24:46 +0200

    Added sample tab app and the configuration and removal pages for that application.

---
+++ TeamsTabAppServer/Shared/TeamsContextView.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Blazorade.Teams.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsTabAppServer.Shared
+{
+    partial class TeamsContextView
+    {
+
+        [Parameter]
+        public ApplicationContext Context { get; set; }
+
+    }
+
+}
