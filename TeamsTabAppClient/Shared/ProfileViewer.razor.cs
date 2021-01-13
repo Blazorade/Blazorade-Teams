9a78fa104a60ef8e271a68d7136393a86cb9a85e
Mika Berglund <mika.berglund@meteoriitti.com>
2021-01-13 14:36:02 +0200

    Finished the WebAssembly demo app.

---
+++ TeamsTabAppClient/Shared/ProfileViewer.razor.cs
@@ -0,0 +1,54 @@
+﻿using Blazorade.Teams.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TeamsTabAppClient.Shared
+{
+    partial class ProfileViewer
+    {
+
+        [Parameter]
+        public ApplicationContext Context { get; set; }
+
+        [Parameter]
+        public UserProfileModel Model { get; set; } = new UserProfileModel();
+
+        private static HttpClient Client = new HttpClient();
+
+        protected async override Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+
+            var authResult = await this.Context.TeamsInterop.Authentication.GetAuthenticationResultAsync(this.Context.Context);
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            var response = await Client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                this.Model = JsonSerializer.Deserialize<UserProfileModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+        }
+    }
+
+    public class UserProfileModel
+    {
+        public string DisplayName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Mail { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string UserPrincipalName { get; set; }
+
+    }
+}
