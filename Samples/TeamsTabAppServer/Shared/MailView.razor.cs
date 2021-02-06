35c0f090299e3554b3d9bf5600c97374455752d9
Mika Berglund <mika.berglund@meteoriitti.com>
2021-02-06 23:55:00 +0200

    Improved the server demo app to include multiple personal tabs, with different scopes.

---
+++ Samples/TeamsTabAppServer/Shared/MailView.razor.cs
@@ -0,0 +1,30 @@
+﻿using Blazorade.Teams.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsTabAppServer.Shared
+{
+    partial class MailView
+    {
+        [Parameter]
+        public ApplicationContext Context { get; set; }
+
+        [Parameter]
+        public IEnumerable<Message> Messages { get; set; }
+
+
+
+        protected async override Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+
+            var authProvider = new AuthenticationProvider(this.Context, this.MsalService);
+            var client = new GraphServiceClient(authProvider);
+            this.Messages = await client.Me.MailFolders.Inbox.Messages.Request().GetAsync();
+        }
+    }
+}
